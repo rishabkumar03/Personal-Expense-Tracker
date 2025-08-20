a6ef2f196a3a9947837eb31c77ce138e43d16c26
rishabkumar03 <rishabkumar3.2005@gmail.com>
2025-08-21 02:03:08 +0530

    Implement data persistence with JSON for expense tracking

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
+# Phase 2: Data persistence
+
+# Save/load from JSON file,
+# Basic error handling.
+
+import json
+
+def load_data():
+    try:
+        with open('expenses.txt', 'r') as file:
+            demo = json.load()
+            return demo
+    except FileNotFoundError:
+            return[]
+
+def save_data(expenses):
+    try:
+        with open('expenses.txt', 'w') as file:
+            json.dump(expenses, file, indent=2)
+            print("Data saved successfully!")
+    except Exception as e:
+        print(f"Error while saving the data: {e}")        
+
 # Phase 1: Core functionality
 
 # Add expenses with amount, category, description, date,
@@ -10,6 +33,7 @@ def add_expenses(expenses):
     description = input("Enter description: ")
     date = input("Enter date: ")
     expenses.append({'amount': amount, 'category': category, 'description': description, 'date': date})
+    save_data(expenses)
 
 def list_all_expenses(expenses):
     if not expenses:
@@ -22,7 +46,7 @@ def list_all_expenses(expenses):
         print(f"{index}. Amount: {expense['amount']}, Category: {expense['category']}, Description: {expense['description']}, Date: {expense['date']}")
 
 def main():
-    expenses = []
+    expenses = load_data()
     while True:
         print("\n ^^^^^^^^^^^^^^ PERSONAL EXPENSE TRACKER ^^^^^^^^^^^^^^")
         print("Choose an option from below")
@@ -44,11 +68,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# Phase 2: Data persistence
-
-# Save/load from JSON file,
-# Basic error handling.
 
 # Phase 3: Analysis features
 
