9105bcbc5ed786e7f64be585fe3b9dd138baa588
rishabkumar03 <rishabkumar3.2005@gmail.com>
2025-08-26 01:46:07 +0530

    Enhance expense tracking with monthly summaries and update expense data format

--- app.py
+++ app.py
@@ -5,10 +5,12 @@
 
 import json
 
+from datetime import datetime
+
 def load_data():
     try:
         with open('expenses.txt', 'r') as file:
-            demo = json.load()
+            demo = json.load(file)
             return demo
     except FileNotFoundError:
             return[]
@@ -19,7 +21,41 @@ def save_data(expenses):
             json.dump(expenses, file, indent=2)
             print("Data saved successfully!")
     except Exception as e:
-        print(f"Error while saving the data: {e}")        
+        print(f"Error while saving the data: {e}")  
+
+# Phase 3: Analysis features
+
+# Monthly spending summaries,
+# Category-wise breakdown,
+# Budget tracking.
+
+def monthly_expense(expenses):
+    if not expenses:
+        print("No expenses to summarize.")
+        return
+    
+    monthly_totals = {}
+
+    for expense in expenses:
+        try:
+            date_obj = datetime.strptime(expense['date'], "%d-%m-%Y")
+            month_year = date_obj.strftime("%B %Y")
+
+            if month_year in monthly_totals:
+                monthly_totals[month_year] += expense['amount']
+            else:
+                monthly_totals[month_year] = expense['amount']
+
+        except ValueError:
+            print(f"Error while calcultaing monthly expense")
+
+    print("\n")
+    print("^^^^^^^^^^^^^^ MONTHLY SPENDING SUMMARY ^^^^^^^^^^^^^^")
+    for month, total in monthly_totals.items():
+        print(f"{month}: ${total:.2f}")
+    print("\n")
+    print("*" *70)
+    
 
 # Phase 1: Core functionality
 
@@ -44,6 +80,7 @@ def list_all_expenses(expenses):
     print("*" *70)
     for index, expense in enumerate(expenses, start=1):
         print(f"{index}. Amount: {expense['amount']}, Category: {expense['category']}, Description: {expense['description']}, Date: {expense['date']}")
+    print("*" *70)
 
 def main():
     expenses = load_data()
@@ -52,7 +89,8 @@ def main():
         print("Choose an option from below")
         print("1. List all Expenses")
         print("2. Add an Expense")
-        print("3. Exit") 
+        print("3. Monthly Summaries")
+        print("4. Exit") 
         choice = input("Enter your choice: ")
 
         match choice: 
@@ -61,19 +99,15 @@ def main():
             case '2':
                 add_expenses(expenses)
             case '3':
+                monthly_expense(expenses)
+            case '4':
                 break
             case _:
-                print("Invalid Choice")
+                print("\n Invalid Choice")
                 
 
 if __name__ == "__main__":
     main()
-
-# Phase 3: Analysis features
-
-# Monthly spending summaries,
-# Category-wise breakdown,
-# Budget tracking.
 
 # Phase 4: GUI (Optional)
 
