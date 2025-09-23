3c585731ae67f82b2b917d68b68e2c84729bbbfb
rishabkumar03 <rishabkumar3.2005@gmail.com>
2025-09-24 01:27:25 +0530

    Implement recurring expenses management features including add, delete, and list functionalities with unknown bugs.

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def load_data():
             demo = json.load(file)
             return demo
     except FileNotFoundError:
-            return[]
+            return []
 
 def save_data(expenses):
     try:
@@ -149,8 +149,7 @@ def show_category_chart(expenses):
 # Phase 5: Advanced features
 
 # Export to CSV,
-# Recurring expenses,
-# Multiple accounts.
+# Recurring expenses.
 
 def export_to_csv(expenses):
     if not expenses:
@@ -174,6 +173,150 @@ def export_to_csv(expenses):
             print("Data exported to 'expenses_export.csv' successfully!")
     except Exception as e:
         print(f"Error exporting to excel {e}")
+
+def load_recurring_data():
+    try:
+        with open('recurring_expenses.txt', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+def save_recurring_data(recurring_expenses):
+    try:
+        with open('recurring_expenses.txt', 'w') as file:
+            json.dump(recurring_expenses, file, indent=2)
+            print("Recurring data saved successfully")
+    except Exception as e:
+        print(f"Error while saving the recurring data: {e}")
+
+def add_recurring_data(recurring_expenses):
+    print("\n ADD RECURRING EXPENSES")
+
+    name = input("Enter recurring data name: ")
+    amount = float(input("Enter recurring data amount: "))
+    category = input("Enter recurrnig data category: ")
+    description = input("Enter recurring data description: ")
+
+    print("\n FREQUENCY OPTIONS")   
+    print("1. Monthly")
+    print("2. Weekly")
+
+    freq_choice = input("Enter frequency choice: ")
+
+    if freq_choice == '1':
+        frequency = 'Monthly'
+        day = int(input("Enter day of month (1-31): "))
+        recurring_expenses.append({
+            'name': name,
+            'amount': amount,
+            'category': category,
+            'description': description,
+            'frequency': frequency,
+            'day': day
+        })
+    
+    elif freq_choice == '2':
+        frequency = 'Weekly'
+        day = input("Enter day of week (Monday, Tuesday, etc.)")
+        recurring_expenses.append({
+            'name': name,
+            'amount': amount,
+            'category': category,
+            'description': description,
+            'frequency': frequency,
+            'day': day
+        })
+
+    else:
+        print("Only monthly and weekly recurring expenses are supported for now")
+        return
+    
+    save_recurring_data(recurring_expenses)
+    print(f"Recurring Expense '{name}' Added!")
+
+def delete_recurring_data(recurring_expenses):
+    list_all_recurring_data(recurring_expenses)
+    recurring_index = int(input("Enter the recurring expense to delete: "))
+    if 1 <= recurring_index <= len(recurring_expenses):
+        del recurring_expenses[recurring_index-1]
+        save_recurring_data(recurring_expenses)
+    else: 
+        print("Invalid recurring index selected")
+
+def list_all_recurring_data(recurring_expenses):
+    if not recurring_expenses:
+        print("No recurring expenses found")
+        return
+    
+    print("\n")
+    print("*" *70)
+    for recurring_index, recurring_expenses in enumerate(recurring_expenses, start=1):
+        print(f"{recurring_index}, Name: {recurring_expenses['name']}, Amount: {recurring_expenses['amount']}, Category: {recurring_expenses['category']}, Description: {recurring_expenses['description']}, Frequency: {recurring_expenses['frequency']}, Day: {recurring_expenses['day']}")
+    print("*" *70)
+
+def process_recurring_data(expenses, recurring_expenses):
+    if not recurring_expenses:
+        return
+    
+    today = datetime.now()
+    current_day = today.day
+    current_weekday = today.strftime("%A")
+
+    added_count = 0
+
+    for recurring in recurring_expenses:
+        should_add = False
+
+        if recurring['frequency'] == 'Monthly' and recurring['day'] == current_day:
+            already_added = False
+            for expense in expenses:
+                expense_date = datetime.strptime(expense['date'], "%d-%m-%Y")
+                if (expense['description'] == f"{recurring['description']}" and expense_date.month == today.month and expense_date.year == today.year):
+                    already_added = True
+                    break
+
+            if not already_added:
+                should_add = True
+
+        elif recurring['frequency'] == 'Weekly' and recurring['day'] == current_weekday:
+            should_add = True
+
+        if should_add:
+            expenses.append({
+                'amount': recurring['amount'],
+                'category': recurring['category'],
+                'description': recurring['description'],
+                'date': today.strftime("%d-%m-%Y")
+            })
+            added_count += 1
+
+    if added_count > 0:
+        save_data(expenses)
+        print(f"Added {added_count} recurring expense successfully")
+    
+    return expenses
+
+def manage_recurring_data(recurring_expenses):
+    while True:
+        print("\n ^^^^^^^^^^^^^^^^ RECCURRING EXPENSES ^^^^^^^^^^^^^^^^")
+        print("Choose an option from below")
+        print("1. List all recurring data")
+        print("2. Add a recurring data")
+        print("3. Delete a recurring data")
+        print("4. Exit")
+        choice = input("Enter your choice: ")
+
+        match choice:
+            case '1':
+                list_all_recurring_data(recurring_expenses)
+            case '2':
+                add_recurring_data(recurring_expenses)
+            case '3':
+                delete_recurring_data(recurring_expenses)
+            case '4':
+                break
+            case _:
+                print("\n Invalid choice")
 
 # Phase 1: Core functionality
 
@@ -224,6 +367,9 @@ def list_all_expenses(expenses):
 
 def main():
     expenses = load_data()
+    recurring_expenses = load_recurring_data()
+    expenses = process_recurring_data(expenses, recurring_expenses)
+
     while True:
         print("\n ^^^^^^^^^^^^^^ PERSONAL EXPENSE TRACKER ^^^^^^^^^^^^^^")
         print("Choose an option from below")
@@ -236,7 +382,8 @@ def main():
         print("7. Monthly Summary Chart")
         print("8. Category Summary Chart")
         print("9. Export to CSV") 
-        print("10. Exit")
+        print("10. Manage Recurring Expenses")
+        print("11. Exit")
         choice = input("Enter your choice: ")
 
         match choice: 
@@ -259,10 +406,11 @@ def main():
             case '9':
                 export_to_csv(expenses)
             case '10':
+                manage_recurring_data(recurring_expenses)
+            case '11':
                 break
-            case '_':
+            case _:
                 print("\n Invalid Choice")
                 
-
 if __name__ == "__main__":
     main()
